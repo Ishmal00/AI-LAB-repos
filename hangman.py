5f446d57f7b7a43292430711314b4cc91010a30c
Ishmal00 <iznasili00@gmail.com>
2025-08-25 22:57:21 +0500

    hangman.py

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 import random
 import hangman_stages
-List = ['apple', 'banana', 'cherry', 'date', 'strawberry']
+List = ['furiously', 'intelligence', 'technology', 'information', 'strawberry']
+# List = ['apple', 'banana', 'cherry', 'date', 'strawberry']
 lives = 8
 choosen_word = random.choice(List)
 print(choosen_word)
@@ -37,52 +38,3 @@ while not game_over:
 
 
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# game_over = False
-# while not game_over:  
-#     guess = input("Guess a letter: ").lower()
-#     if guess in display:
-#         print(f"You have already guessed {guess}")
-#     for position in range(len(choosen_word)):
-#         letter = choosen_word[position]
-#         if letter == guess:
-#             display[position] = letter
-#     if guess not in choosen_word:
-#         lives -= 1
-#         print(f"You guessed {guess}, that's not in the word. You lose a life.")
-#         if lives == 0:
-#             game_over = True
-#             print("You lose.")
-#     print(f"{' '.join(display)}")
-#     if "_" not in display:
-#         game_over = True
-#         print("You win.")
-#     print(f"You have {lives} lives left.")
